bfffdf4e4a7e361636247248e2e6cf9b3188fe47
phucnn <phuc123vodanh@gmail.com>
2025-11-18 09:00:39 +0700

    Update PY01020.py

--- PY01020.py
+++ PY01020.py
@@ -1,8 +1,5 @@
 t=int(input())
 for _ in range(t):
     n=input()
-    sum=0
-    ok = True
-    for i in range(0,len(n)-1):
-        sum+=a[i]
-        if a[i]
+    if n[-2:] == '86': print('YES')
+    else: print('NO')
